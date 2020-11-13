0a02643f7d0c43dd41fa14cfca7bebabe1bcb533
luuchannam3 <namchanluu3@gmail.com>
2020-11-13 11:36:06 +0700

    java

---
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -0,0 +1,10 @@
+package tacos.data;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.Ingredient;
+
+public interface IngredientRepository  extends CrudRepository<Ingredient, String> {
+}
---
+++ src/main/java/tacos/data/TacoRepository.java
@@ -0,0 +1,11 @@
+package tacos.data;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.Taco;
+
+public interface TacoRepository  extends CrudRepository<Taco, String> {
+	
+}
